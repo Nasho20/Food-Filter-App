57b1dc219a4fa4b956bdf50fbf88e9417d10ebba
Nasho20 <thanasbaba@gmail.com>
2023-03-11 20:54:08 +0100

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from "./fakeData/Data";
+import "./App.css";
+import Card from "./Card";
 
 function App() {
+  const [food, setFood] = useState(Data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <h1 className="col-12 text-center my-3 fw-bold">Today's Menu</h1>
+        <Card food={food} />
+      </div>
     </div>
   );
 }
