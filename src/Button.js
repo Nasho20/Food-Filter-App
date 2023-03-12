32efc41477f3eed6e012090452464c27af644c19
Nasho20 <thanasbaba@gmail.com>
2023-03-12 01:13:01 +0100

    First steps of showing the Food Cards.

---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import Data from "./fakeData/Data";
+import React from "react";
+
+export default function Buttons({ setFood }) {
+  return <div>Buttons</div>;
+}
--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default function Card() {
-  return <div className="container-fluid">Card</div>;
+export default function Card({ food }) {
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        {food.map((value) => {
+          return (
+            <div
+              className="col-md-4 col-sm-6 my-3 py-3 border-0"
+              key={value.id}
+            >
+              <div className="card-img-top text-center">
+                <img src={value.img} alt={value.title} className="photo w-75" />
+              </div>
+              <div className="card-body">
+                <div className="card-title fw-bold fs-4">
+                  {value.title}
+                  {value.price}
+                </div>
+                <div className="card-text">{value.desc}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
